9b1f5d87d5c4375f1f23dea6cfadcfabc43f5ecd
Samuel Harden <samuel2.harden@live.uwe.ac.uk>
2017-09-26 16:07:19 +0000

    First Commit
    Player controls and Initial Raycast done

---
+++ Assets/Scripts/CameraMouseLook.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CameraMouseLook : MonoBehaviour
+{
+    public float camSpeedVertical = 2.0f;
+    private float pitch;
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        pitch = 0.0f;
+	}
+	
+
+
+	// Update is called once per frame
+	void Update ()
+    {
+        pitch -= camSpeedVertical * Input.GetAxis("Mouse Y");
+
+        transform.localEulerAngles = new Vector3(pitch, 0.0f, 0.0f);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 10.0f;
+    public float camSpeedHorizontal = 2.0f;
+    private float yaw;
+
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        yaw = 0.0f;
+
+        // Hide Cursor
+        Cursor.visible = false;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // update player position
+        updateInput();
+
+        // update camera horizontal Rotation
+        updateCamera();
+    }
+
+
+
+    void updateInput()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveForward();
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveBack();
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveLeft();
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveRight();
+        }
+
+        // Not movement related - used for Debugging
+        if (Input.GetKey(KeyCode.Alpha0))
+        {
+            Cursor.visible = true;
+        }
+    }
+
+
+
+    void updateCamera()
+    {
+        yaw += camSpeedHorizontal * Input.GetAxis("Mouse X");
+
+        transform.eulerAngles = new Vector3(0.0f, yaw, 0.0f);
+    }
+
+
+
+    void moveForward()
+    {
+        transform.position += transform.forward * Time.deltaTime * movementSpeed;
+    }
+
+
+
+    void moveBack()
+    {
+        GetComponent<Rigidbody>().position += -transform.forward * Time.deltaTime * movementSpeed;
+    }
+
+
+
+    void moveLeft()
+    {
+        GetComponent<Rigidbody>().position += -transform.right * Time.deltaTime * movementSpeed;
+    }
+
+
+
+    void moveRight()
+    {
+        GetComponent<Rigidbody>().position += transform.right * Time.deltaTime * movementSpeed;
+    }
+
+}
